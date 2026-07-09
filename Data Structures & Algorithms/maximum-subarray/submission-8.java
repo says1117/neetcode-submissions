f2864405b3fafbd2e82778afc1db140042721799
says1117 <saysschool4321@gmail.com>
2026-07-09 04:50:42 -0400

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int n = nums.length;
+        int curThread = 0;
+
+        if(n == 1) return nums[0];
+
+        for(int i=0; i<nums.length; i++){
+
+            if(curThread < 0){
+                curThread = nums[i];
+            }
+            else{
+                curThread = curThread + nums[i];
+            }
+            maxSum = Math.max(maxSum, curThread);
+        }
+        return maxSum;
+    }
+}
