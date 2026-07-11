1fe6ec742fe42ce9035deedc7d3899592b384cef
says1117 <saysschool4321@gmail.com>
2026-07-11 05:26:11 -0400

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int mostIdx = 0;
+
+        if(Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) return -1;
+
+        for(int i=0; i < gas.length; i++){
+            totalGas = totalGas + gas[i] - cost[i];
+            if(totalGas < 0){
+                totalGas = 0;
+                mostIdx = i+1;
+            }
+        }
+        return mostIdx;
+    }
+}
