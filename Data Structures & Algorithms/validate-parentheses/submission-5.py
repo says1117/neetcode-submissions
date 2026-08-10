7e0331c2bdcbcc6843aeb1e6d3bbff58bac3b956
says1117 <saysschool4321@gmail.com>
2026-08-10 02:39:57 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        res = []
+        cur =''
+        for char in s:
+            if char == '(' or char == '[' or char == '{':
+                res.append(char)
+            else:
+                if res:
+                    cur = res.pop()
+                    if cur == '(' and char != ')':
+                        return False
+                    if cur == '{' and char != '}':
+                        return False
+                    if cur == '[' and char != ']':
+                        return False
+                else:
+                    return False
+
+        if res:
+            cur = res.pop()
+            if cur == '(' or cur == ')' or cur == '[' or cur == ']' or cur == '{' or cur == '}':
+
+                return False
+        return True
